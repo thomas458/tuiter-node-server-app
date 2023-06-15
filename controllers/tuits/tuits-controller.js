abc9e5bfa63401370e74f6d10c16b53a066d9f0c
Tom Hao <tomhao@Toms-MacBook-Pro.local>
2023-06-15 09:49:25 -0700

    fixed render

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -26,6 +26,7 @@ const createTuit = async (req, res) => {
 }
 const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
     // tuits = tuits.filter((t) =>
     //     t._id !== tuitdIdToDelete);
     res.json(status);
