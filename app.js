e46995b63d384c2964dc378409eaf5a93179ddf8
Tom Hao <tomhao@Toms-MacBook-Pro.local>
2023-06-15 09:30:13 -0700

    render fix

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import cors from 'cors'
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb+srv://chenyanghao615:ITLkai2a5i9vFfMY@cluster0.75nvzso.mongodb.net/');
+mongoose.connect('mongodb+srv://chenyanghao615:ITLkai2a5i9vFfMY@cluster0.75nvzso.mongodb.net/tuiter?retryWrites=true&w=majority');
+
 //const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://chenyanghao615:ITLkai2a5i9vFfMY@cluster0.75nvzso.mongodb.net/'
 //mongoose.connect(CONNECTION_STRING);
 
